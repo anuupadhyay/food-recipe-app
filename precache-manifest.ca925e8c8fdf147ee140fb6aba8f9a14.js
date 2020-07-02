7d73df555c991b5c82846297376fbff81a93221f
Anu Upadhyay <anu.upadhyay.1994@gmail.com>
2020-07-02 12:27:19 +0530

    Updates

---
+++ precache-manifest.ca925e8c8fdf147ee140fb6aba8f9a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57de76b27dc8de888de0518d67081a3",
+    "url": "/food-recipe-app/index.html"
+  },
+  {
+    "revision": "20a491fdc8289a3783f8",
+    "url": "/food-recipe-app/static/css/main.dc819ff3.chunk.css"
+  },
+  {
+    "revision": "61096c93d70b4cbd505d",
+    "url": "/food-recipe-app/static/js/2.67bd9ed8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe-app/static/js/2.67bd9ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a491fdc8289a3783f8",
+    "url": "/food-recipe-app/static/js/main.f4f746fc.chunk.js"
+  },
+  {
+    "revision": "95ae9d36db7424bb2ad3",
+    "url": "/food-recipe-app/static/js/runtime-main.0690d01c.js"
+  }
+]);
